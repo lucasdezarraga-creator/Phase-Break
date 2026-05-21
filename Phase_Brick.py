66a70b835ec5643147034e942d9bb7646031ca68
lucasdezarraga-creator <lucasdezarraga@gmail.com>
2026-05-21 17:37:23 -0400

    I have given the brick collison. Now to make the brick break when the ball hits it.

--- Phase_Brick.py
+++ Phase_Brick.py
@@ -16,6 +16,8 @@ paddle.centerx = screen.get_width() / 2
 ball_pos = pygame.Vector2(paddle.centerx, paddle.top - BALL_RADIUS)
 ball_velo = pygame.Vector2(0,0)
 
+brick = pygame.Rect(100, 100, 120, 40)
+
 was_launched = False
 
 while running:
@@ -62,6 +64,27 @@ while running:
             ball_pos.y = paddle.y - BALL_RADIUS
             ball_velo.y *= -1
 
+        if ball_rect.colliderect(brick):
+            offset_left = ball_rect.right - brick.left
+            offset_right = brick.right - ball_rect.left 
+            offset_top = ball_rect.bottom - brick.top
+            offset_bottom = brick.bottom - ball_rect.top
+
+            smallest_offest = min(offset_left, offset_right, offset_top, offset_bottom)
+
+            if smallest_offest == offset_left:
+                ball_pos.x = brick.left - BALL_RADIUS
+                ball_velo.x *= -1
+            elif smallest_offest == offset_right:
+                ball_pos.x = brick.right + BALL_RADIUS
+                ball_velo.x *= -1
+            elif smallest_offest == offset_top:
+                ball_pos.y = brick.top - BALL_RADIUS
+                ball_velo.x *= -1
+            elif smallest_offest == offset_bottom:
+                ball_pos.y = brick.bottom + BALL_RADIUS
+                ball_velo.y *= -1
+
     if paddle.left < 0:
         paddle.left = 0
     elif paddle.right > screen.get_width():
@@ -69,6 +92,7 @@ while running:
 
     screen.fill((53, 91, 156))
     pygame.draw.rect(screen, (51, 171, 44), paddle)
+    pygame.draw.rect(screen, (255, 0, 0), brick)
     pygame.draw.circle(screen, (136, 27, 179), ball_pos, BALL_RADIUS)
 
     pygame.display.flip()
