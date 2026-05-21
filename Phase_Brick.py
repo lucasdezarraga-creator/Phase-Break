3d5bbce1f3177dc76448a46ab7f39fcba63fa91f
lucasdezarraga-creator <lucasdezarraga@gmail.com>
2026-05-21 16:23:04 -0400

    Added a ball that sits in the middle of the paddle. I also made the paddle sit in the middle of the screen.

--- Phase_Brick.py
+++ Phase_Brick.py
@@ -6,7 +6,16 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-paddle = pygame.Rect(screen.get_width() / 2, 610, 250, 30)
+PLAYER_SPEED = 450
+BOUNCE_ANGLE = 45
+BALL_RADIUS = 25
+
+paddle = pygame.Rect(0, 610, 250, 30)
+paddle.centerx = screen.get_width() / 2
+
+ballpos = pygame.Vector2(paddle.centerx, paddle.top - BALL_RADIUS)
+
+was_launched = False
 
 while running:
     for event in pygame.event.get():
@@ -16,18 +25,27 @@ while running:
     screen.fill((53, 91, 156))
 
     pygame.draw.rect(screen, (51, 171, 44), paddle)
+    pygame.draw.circle(screen, (136, 27, 179), ballpos, BALL_RADIUS)
 
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        paddle.x -= 300 * dt
+        paddle.x -= PLAYER_SPEED * dt
+        if not was_launched:
+            ballpos.x = paddle.x + (paddle.width / 2)
+
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        paddle.x += 300 * dt
+        paddle.x += PLAYER_SPEED * dt
+        if not was_launched:
+            ballpos.x = paddle.x + (paddle.width / 2)
+    if keys[pygame.K_SPACE]:
+        was_launched = True
 
     if paddle.left < 0:
-        paddle.left = -0
+        paddle.left = 0
     elif paddle.right > screen.get_width():
         paddle.right = screen.get_width()
+
 
     pygame.display.flip()
 
